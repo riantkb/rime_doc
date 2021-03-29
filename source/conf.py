99964adfe9ec300c14046725c6162c04c41b85d3
riantkb <kubota.rian@gmail.com>
2021-03-30 07:34:27 +0900

    fix behavior

--- source/conf.py
+++ source/conf.py
@@ -43,7 +43,7 @@ language = 'ja'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['index_old.rst']
 
 
 # -- Options for HTML output -------------------------------------------------
